dddb5362050cdb08fcbdd33be9f6676bf95e614d
J-Sheerin-Farhana <sheerinfj51011@gmail.com>
2025-09-26 00:02:24 +0530

    Spring_Boot_Project with Placement module

---
+++ DI/DI/src/main/java/com/IOC/DI/DiApplication.java
@@ -0,0 +1,13 @@
+package com.IOC.DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiApplication.class, args);
+	}
+
+}
